6903ed50ef37641a5708a01ecc97bd1e1508b990
Jaswanth <jaswanthpulavarthi2007@gmail.com>
2026-08-28 23:08:18 +0530

    Add voting eligibility check

---
+++ src/main/java/session1/assigment_problems/level1/VotingEligibilityCheck.java
@@ -0,0 +1,28 @@
+
+package main.java.session1.assigment_problems.level1;
+
+import java.util.Scanner;
+
+public class VotingEligibilityCheck {
+
+    static void checkVotingEligibility(int age) {
+        boolean eligible = age >= 18;
+
+        if (eligible) {
+            System.out.println("Eligible to vote");
+        } else {
+            System.out.println("Not eligible to vote");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter age: ");
+        int age = sc.nextInt();
+
+        checkVotingEligibility(age);
+
+        sc.close();
+    }}
+
