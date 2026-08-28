db4f6db66d8aa4f145d030c5e84dd361e0fb2038
Jaswanth <jaswanthpulavarthi2007@gmail.com>
2026-08-29 00:07:11 +0530

    Add prime number class problem

---
+++ src/main/java/session1/class_problems/PrimeNumber.java
@@ -0,0 +1,36 @@
+package main.java.session1.class_problems;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        boolean isPrime = true;
+
+        if (number <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i < number; i++) {
+
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println("Is the number " + number + " a Prime number? Yes");
+        } else {
+            System.out.println("Is the number " + number + " a Prime number? No");
+        }
+
+        scanner.close();
+    }
+}
