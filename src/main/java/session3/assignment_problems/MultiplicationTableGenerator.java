3d3c0c5d316348f1facae72e87c27243a98f1b7e
Jaswanth <jaswanthpulavarthi2007@gmail.com>
2026-08-29 21:31:38 +0530

    Add Session 3 problems

---
+++ src/main/java/session3/assignment_problems/MultiplicationTableGenerator.java
@@ -0,0 +1,46 @@
+
+package session3.assignment_problems;
+
+import java.util.Scanner;
+
+public class MultiplicationTableGenerator {
+
+    static void generateFirstValidTable(int[] candidates) {
+
+        for (int i = 0; i < candidates.length; i++) {
+
+            int number = candidates[i];
+
+            if (number < 1) {
+                System.out.println("Skipping invalid number: " + number);
+                continue;
+            }
+
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(number + " x " + j + " = " + (number * j));
+            }
+
+            break;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of candidates: ");
+        int n = sc.nextInt();
+
+        int[] candidates = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter candidate " + (i + 1) + ": ");
+            candidates[i] = sc.nextInt();
+        }
+
+        generateFirstValidTable(candidates);
+
+        sc.close();
+    }
+}
+
---
+++ src/main/java/session3/assignment_problems/OddEvenChecker.java
@@ -0,0 +1,29 @@
+
+package session3.assignment_problems;
+
+import java.util.Scanner;
+
+public class OddEvenChecker {
+
+    static void checkOddEven(int number) {
+
+        if (number % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int number = sc.nextInt();
+
+        checkOddEven(number);
+
+        sc.close();
+    }
+}
+
